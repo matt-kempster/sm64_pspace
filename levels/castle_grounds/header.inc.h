e2e447654e1474d1c1cb7e057bbfa04fc935a984
Matt Kempster <kidpixel@gmail.com>
2020-03-24 21:57:44 -0700

    Adding it actually

---
+++ levels/castle_grounds/header.inc.h
@@ -0,0 +1,7 @@
+extern const GeoLayout castle_grounds_area_1_Level[];
+extern const GeoLayout castle_grounds_area_1_level[];
+extern const Collision castle_grounds_area_1_collision[];
+extern const MacroObject castle_grounds_area_1_Area_macro_objs[];
+extern const Gfx castle_grounds_Plane_mesh[];
+extern const Gfx castle_grounds_material_revert_render_settings[];
+

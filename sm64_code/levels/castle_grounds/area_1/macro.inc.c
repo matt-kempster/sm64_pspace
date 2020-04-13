23c06d888bfe5d38e71a4b6c5119d948217a0ac1
Matt Kempster <kidpixel@gmail.com>
2020-04-12 22:45:59 -0700

    Delete low-hanging fruit in castle_grounds

--- sm64_code/levels/castle_grounds/area_1/macro.inc.c
+++
@@ -1,4 +0,0 @@
-const MacroObject castle_grounds_area_1_Area_macro_objs[] = {
-	MACRO_OBJECT_END(),
-};
-
--- sm64_code/levels/castle_grounds/leveldata.inc.c
+++ sm64_code/levels/castle_grounds/leveldata.inc.c
@@ -1,5 +1,4 @@
 #include "levels/castle_grounds/area_1/collision.inc.c"
-#include "levels/castle_grounds/area_1/macro.inc.c"
 #include "levels/castle_grounds/area_1/movtext.inc.c"
 #include "levels/wdw/texture.inc.c"
 #include "levels/wdw/water_level_diamond/model.inc.c"
